055b2711631044f2e01fd46cb824f08c43f6743a
Obiwan1995 <brice.miclo@gmail.com>
2017-11-25 19:55:29 -0500

    Lock menu when the user has started a route #9

--- mobile/src/main/java/brice/explorun/fragments/MapFragment.java
+++ mobile/src/main/java/brice/explorun/fragments/MapFragment.java
@@ -12,6 +12,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -602,6 +603,8 @@ public class MapFragment extends PlacesObserverFragment implements OnMapReadyCal
 
 	public void onRouteStart()
 	{
+		AppCompatActivity activity = (AppCompatActivity) this.getActivity();
+		activity.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 		this.slideDownFragment(this.routeInfoLayout);
 		this.slideUpFragment(this.currentRouteLayout);
 		this.currentRouteFragment.update(this.customRoute.getSportType());
@@ -610,6 +613,8 @@ public class MapFragment extends PlacesObserverFragment implements OnMapReadyCal
 	public void onRouteStop()
 	{
 		this.removePolylines();
+		AppCompatActivity activity = (AppCompatActivity) this.getActivity();
+		activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		this.customRoute = null;
 	}
 }
